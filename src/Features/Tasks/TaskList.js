01d87fd6f9fd8eb7351ec660809afec7eddb9580
El Mehdi <taiielmehdi21@gmail.com>
2022-11-21 21:23:58 +0100

    Add Task

--- src/Features/Tasks/TaskList.js
+++ src/Features/Tasks/TaskList.js
@@ -6,6 +6,11 @@ import { useState } from "react";
 const TaskList = () => {
   const queryClient = useQueryClient();
   const [newName, setNewName] = useState('')
+  const [newUserName, setNewUserName] = useState('')
+  const [newType, setNewType] = useState('')
+
+
+  const [hideList, setHideList] = useState(false)
   const {
     isLoading,
     isError,
@@ -33,50 +38,90 @@ const TaskList = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    addTaskMutation.mutate({ id: 21, Name: newName, UserName: "3582197214794723", Type: "Yearly" })
+    addTaskMutation.mutate({ id: Math.random(), Name: newName, UserName: newUserName, Type: newType })
     setNewName('')
+    setNewUserName('')
+    setNewType('')
+    setHideList(!hideList)
   }
 
   const newItemSection = (
     <form onSubmit={handleSubmit}>
       <div className="row">
-        <div className="col-4">
+        <div className="col-3">
           <input className="form-control"
             type="text"
             id="new-name"
             value={newName}
             onChange={(e) => setNewName(e.target.value)}
-            placeholder="Enter new Task Name"
+            placeholder="Enter new Task Name" required
           />
         </div>
-        <div className="col-4">
+        <div className="col-3">
+          <input className="form-control"
+            type="text"
+            id="new-name"
+            value={newUserName}
+            onChange={(e) => setNewUserName(e.target.value)}
+            placeholder="Enter Assigned To" required
+          />
+        </div>
+        <div className="col-3">
+          <input className="form-control"
+            type="text"
+            id="new-name"
+            value={newType}
+            onChange={(e) => setNewType(e.target.value)}
+            placeholder="Enter Task Type" required
+          />
+        </div>
+        <div className="col-3">
           <button className="btn btn-primary">
-            Submit
+            Create Task
           </button>
         </div>
       </div>
     </form>
   )
-  console.log(tasks)  
-  return (  <main>
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tasks?.map((task) => {
-          <tr key={task.id}>
-            <th scope="row">{task}</th>
-          </tr>
-        })}
-      </tbody>
-    </table>
-  </main>)
+  return (
+    <div>
+      {hideList &&
+        newItemSection
+      }
+
+      {!hideList &&
+        <div>
+          <button className="btn btn-success" onClick={() => setHideList(!hideList)}>
+            New Task
+          </button>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Assigned To</th>
+                <th scope="col">Type</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks?.map((task) =>
+                <tr key={task.id}>
+                  <td>{task.id}</td>
+                  <td>{task.Name}</td>
+                  <td>{task.UserName}</td>
+                  <td>{task.Type}</td>
+                  <td><button type="button" className="btn btn-link">Update</button></td>
+                  <td><button type="button" className="btn btn-link" onClick={() => deleteTaskMutation.mutate({ id: task.id })}>Delete</button></td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      }
+    </div>
+  )
 }
 
 export default TaskList;
